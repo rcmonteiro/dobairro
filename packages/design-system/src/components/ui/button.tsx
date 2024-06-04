ca6c2d93e8f3f78abe9d9ba7ae16e4d99d3c3526
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-06-04 14:05:19 -0300

    chore: fix eslint config for design-system package

--- packages/design-system/src/components/ui/button.tsx
+++ packages/design-system/src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { tv, type VariantProps } from 'tailwind-variants'
 const buttonVariants = tv({
   base: [
     'rounded-full py-2 px-6 ring-offset-background flex justify-between gap-2 items-center whitespace-nowrap flex-nowrap',
-    'hover:ring-4 transition ease-in-out duration-200'
+    'hover:ring-4 transition ease-in-out duration-200',
   ],
 
   variants: {
@@ -19,7 +19,8 @@ const buttonVariants = tv({
       primary: 'bg-primary text-white ring-primary/20',
       secondary: 'bg-default text-white ring-secondary/20',
       ghost: 'text-default ring-default/20',
-      outline: 'text-default bg-white border border-default/50 ring-secondary/20',
+      outline:
+        'text-default bg-white border border-default/50 ring-secondary/20',
     },
   },
 
@@ -39,7 +40,11 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ asChild = false, size, variant, className, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button'
     return (
-      <Comp ref={ref} {...props} className={buttonVariants({ size, variant, className })} />
+      <Comp
+        ref={ref}
+        {...props}
+        className={buttonVariants({ size, variant, className })}
+      />
     )
   },
 )
